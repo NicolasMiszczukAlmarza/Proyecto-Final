bc130674cc551126f70a372460ade1371ab0be7a
NicolasMiszczukAlmarza <nicomovil1994@gmail.com>
2025-02-18 14:23:05 +0100

    Usuario registrado, logueado y que ha podido acceder al carrito 2

---
+++ react-tienda/src/components/Carrito.jsx
@@ -0,0 +1,14 @@
+// ../react-tienda/src/components/Carrito.jsx
+
+import React from 'react';
+import './Carrito.css';  // Importa el archivo de estilos del carrito
+
+const Carrito = () => {
+  return (
+    <div>
+      <h1>Esto es un carrito</h1>
+    </div>
+  );
+}
+
+export default Carrito;
