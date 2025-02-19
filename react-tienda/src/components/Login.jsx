7793844328744828dfce391842fb45a28dad307e
NicolasMiszczukAlmarza <nicomovil1994@gmail.com>
2025-02-19 12:16:12 +0100

    Estructura basica carrito hecha, componentes hechos y cerrar sesion hechos

--- react-tienda/src/components/Login.jsx
+++ react-tienda/src/components/Login.jsx
@@ -14,26 +14,23 @@ const Login = () => {
     e.preventDefault();
     console.log('Correo:', email);
     console.log('Contraseña:', password);
+
+    // Guardamos el correo en localStorage
+    localStorage.setItem('userEmail', email);
+
     navigate('/carrito'); // Redirige al carrito después del login
   };
 
   return (
     <div className="container-fluid vh-100 d-flex align-items-center justify-content-center bg-light">
       <div className="row w-75 shadow-lg p-4 bg-white rounded">
-        {/* Columna de la imagen - Mover a la izquierda */}
         <div className="col-md-6 d-flex align-items-center justify-content-center">
-          {/* Cambia la ruta de la imagen a una URL estática desde la carpeta public */}
           <img src="/img/logo/logo.JPG" alt="Logo" className="img-fluid rounded shadow" style={{ maxWidth: '80%' }} />
         </div>
-
-        {/* Columna del formulario - Mover a la derecha */}
         <div className="col-md-6">
           <div className="col-12 text-center mb-4">
-            <h2 className="fw-bold" style={{ color: '#000' }}>
-              Iniciar Sesión
-            </h2>
+            <h2 className="fw-bold" style={{ color: '#000' }}>Iniciar Sesión</h2>
           </div>
-
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Correo electrónico</label>
@@ -59,7 +56,6 @@ const Login = () => {
             </div>
             <button type="submit" className="btn btn-primary w-100">Iniciar Sesión</button>
           </form>
-
           <div className="text-center mt-3">
             <p>¿No tienes cuenta? <Link to="/registro">¡Créala aquí!</Link></p>
           </div>
