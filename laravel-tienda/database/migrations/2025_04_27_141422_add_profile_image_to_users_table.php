4b09f402343d240f1829362a557959cb368df59e
NicolasMiszczukAlmarza <nicomovil1994@gmail.com>
2025-04-27 16:18:54 +0200

    Imagen añadida a la base de datos

---
+++ laravel-tienda/database/migrations/2025_04_27_141422_add_profile_image_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Agregar el campo profile_image con valor por defecto
+            $table->string('profile_image')->nullable()->default('public/img/usuario/principal.png');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Eliminar el campo profile_image
+            $table->dropColumn('profile_image');
+        });
+    }
+};
